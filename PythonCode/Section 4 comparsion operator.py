912a39de74c79665c4f5485c69b967ebeeeae466
== <==>
2020-04-03 01:03:45 +0200

    A dding part nr 31 (Comparison Operators) and part nr 32 (Logical Operators in Python)

--- PythonCode/Section 4 comparsion operator.py
+++ PythonCode/Section 4 comparsion operator.py
@@ -1,1 +1,28 @@
 # # # # # # # # # #  part 31 (Comparison Operators in Python) # # # # # # # # #
+
+""" 
+2 == 2 
+'Bye' == 'bye'
+3 != 3 # not equal
+1>2
+2<5
+2<=2
+5>=4
+print('Bye' == 'bye')
+ """
+
+# # # # # # # # # #  part 32 (Logical Operators in Python) # # # # # # # # #
+# AND , OR , NOT
+
+1 < 2 
+2 < 3
+1 < 2 < 3 # 
+1 < 2 and 2 < 3 # it is the same as line 19
+'h' == 'h' and 2 == 2 
+('h' == 'h') and (2 == 2) #  SAME line 21
+100 == 1 or 2 == 2
+1 == 1
+not(1==1)
+not 1 == 1 # same line 25
+
+
