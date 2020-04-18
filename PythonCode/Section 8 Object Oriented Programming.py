59602c6f84fdaed541667ffad76c516a7c226e12
== <==>
2020-04-18 23:37:09 +0200

    Adding: Section 8: Object Oriented Programming.py file

---
+++ PythonCode/Section 8 Object Oriented Programming.py
@@ -0,0 +1,3 @@
+# ------------------------ Section 8: Object Oriented Programming is from part 59 to 67   -----------------------------
+
+# # # # # # # # # #  part 59 (Object Oriented Programming - Introduction) # # # # # # # # #
