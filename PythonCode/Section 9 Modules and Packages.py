68f627739d2a30ea42107940a9a41e535749c151
== <==>
2020-04-26 14:10:25 +0200

    added: Section 9: Modules and Packages python file

---
+++ PythonCode/Section 9 Modules and Packages.py
@@ -0,0 +1,23 @@
+# ------------------------ Section 9: Modules and Packages is from part 68 to 70   -----------------------------
+
+# # # # # # # # # #  part 68 (Pip Install and PyPi) # # # # # # # # #
+
+
+
+
+
+
+
+
+
+# # # # # # # # # #  part 69 (Modules and Packages) # # # # # # # # #
+
+
+
+
+
+
+
+
+
+# # # # # # # # # #  part 70 ( __name__ and "__main__") # # # # # # # # #
