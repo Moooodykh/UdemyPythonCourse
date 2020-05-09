a6b3983e50a490fc8f67c96b25326181daea30b5
MKHARBOU <eng.mouhammad.kharboutli@gmail.com>
2020-05-09 18:19:25 +0200

    added Built in functions: Section 9 "Built in functions" Map,Reduce,Filter,Zip,Enumerate,all(),any(),complex and Built-in Functions Assessment Test

---
+++ PythonCode/09-Built-in Functions.py
@@ -0,0 +1,20 @@
+# ------------------------ Section 9 "Built in functions" is from part 1 to 9   -----------------------------
+
+# # # # # # # # # #  part 1 (Map Function) # # # # # # # # #
+
+# # # # # # # # # #  part 2 (Reduce  Function) # # # # # # # # #
+
+# # # # # # # # # #  part 3 (Filter Function) # # # # # # # # #
+
+# # # # # # # # # #  part 4 (Zip Function) # # # # # # # # #
+
+# # # # # # # # # #  part 5 (Enumerate Function) # # # # # # # # #
+
+# # # # # # # # # #  part 6 (all() and any() Function) # # # # # # # # #
+
+# # # # # # # # # #  part 7 (Complex Function) # # # # # # # # #
+
+# # # # # # # # # #  part 8 (Built-in Functions Assessment Test) # # # # # # # # #
+
+# # # # # # # # # #  part 9 (Built-in Functions Assessment Test - Solution) # # # # # # # # #
+
