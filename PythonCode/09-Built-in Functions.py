f3e674259956f2ede6a8f7f60487d9e36676e1c6
MKHARBOU <eng.mouhammad.kharboutli@gmail.com>
2020-05-09 18:39:21 +0200

    added part 1 (Map Function)

--- PythonCode/09-Built-in Functions.py
+++ PythonCode/09-Built-in Functions.py
@@ -1,6 +1,28 @@
 # ------------------------ Section 9 "Built in functions" is from part 1 to 9   -----------------------------
 
 # # # # # # # # # #  part 1 (Map Function) # # # # # # # # #
+# map is used to aply iterable(list) to a function.
+
+def ferhernheit(temp):
+    return (9.0/5)*temp + 32
+
+temp_list = [0,22,33,100]
+
+print(list(map(ferhernheit,temp_list)))
+
+# we can used lambda expression 
+print(list(map(lambda n: (9.0/5)*n + 32, temp_list)))
+
+print('*'*100)
+##### map can be used with more than one argument:
+a = [1,2,3]
+b = [4,5,6]
+c = [7,8,9]
+print(list(map(lambda x,y:x+y,a,b)))# taking the a[0] adding to b[0] and so on.
+print(list(map(lambda x,y,z:x+y+z,a,b,c)))# taking the a[0] adding to b[0] adding to c[0] and so on.
+
+
+#--------------------------------------------------------
 
 # # # # # # # # # #  part 2 (Reduce  Function) # # # # # # # # #
 
