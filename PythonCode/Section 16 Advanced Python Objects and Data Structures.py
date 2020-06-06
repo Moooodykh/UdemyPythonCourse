d5be9b2d0ed6b592bbce3d70ff359a94c820dc66
== <==>
2020-06-06 18:18:01 +0200

    added part 103 ( Advanced Strings)

--- PythonCode/Section 16 Advanced Python Objects and Data Structures.py
+++ PythonCode/Section 16 Advanced Python Objects and Data Structures.py
@@ -41,6 +41,66 @@ print(round(3.14233321,2))
 
 # # # # # # # # # #  part 103 ( Advanced Strings) # # # # # # # # #
 
+""" 
+s = 'hello world'
+print(s.capitalize())
+print(s.upper())
+print(s.lower())
+
+# Remember, strings are immutable. None of the above methods change the string in place, they only return modified copies of the original string.
+s = s.upper()
+print(s)
+s= s.lower()
+
+
+#####     Location and Counting
+print(s.count('o')) # returns the number of occurrences, without overlap
+print(s.find('o')) # returns the starting index position of the first occurence
+
+#####     Formatting
+# The center() method allows you to place your string 'centered' between a provided string with a certain length.
+#  Personally, I've never actually used this in code as it seems pretty esoteric...
+
+print(s.center(20,'z'))
+
+# The expandtabs() method will expand tab notations \t into spaces:
+print(
+'hello\thi'.expandtabs()
+)
+
+#####     is check methods
+s = 'hello'
+### isalnum() will return True if all characters in s are alphanumeric
+print(s.isalnum())
+### isalpha() will return True if all characters in s are alphabetic
+print(s.isalpha())
+### islower() will return True if all cased characters in s are lowercase and there is at least one cased character in s, False otherwise.
+print(s.islower())
+# isspace() will return True if all characters in s are whitespace.
+print(s.isspace())
+
+# istitle() will return True if s is a title cased string and there is at least one character in s, i.e.
+# uppercase characters may only follow uncased characters and lowercase characters only cased ones. It returns False otherwise.
+print(s.istitle())
+
+# isupper() will return True if all cased characters in s are uppercase and there is at least one cased character in s, False otherwise.
+print(s.isupper())
+
+# nother method is endswith() which is essentially the same as a boolean check on s-1
+print(s.endswith('o'))
+
+print(s.isidentifier())
+print(s.isascii())
+
+# Built-in Reg. Expressions
+
+###### Strings have some built-in methods that can resemble regular expression operations.
+###  We can use split() to split the string at a certain element and return a list of the results.
+print(s.split('e')) # ['h', 'llo']
+###  We can use partition() to return a tuple that includes the first occurrence of the separator sandwiched between 
+# the first half and the end half.
+print(s.partition('l')) # ('he', 'l', 'lo')
+"""
 
 # # # # # # # # # #  part 104 ( Advanced Sets) # # # # # # # # #
 # # # # # # # # # #  part 105 ( Advanced Dictionaries) # # # # # # # # #
