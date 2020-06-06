fbea20ff742fde661e9c0c56a4a4500b6fa9880c
== <==>
2020-06-06 17:54:04 +0200

    Added section 16.py file Section 16: Advanced Python Objects and Data Structures added part 102 ( Advanced Numbers)

---
+++ PythonCode/Section 16 Advanced Python Objects and Data Structures.py
@@ -0,0 +1,49 @@
+# ------------------------ Section 16 : Section 16: Advanced Python Objects and Data Structures is from part 102 to 108   -----------------------------
+
+# # # # # # # # # #  part 102 ( Advanced Numbers) # # # # # # # # #
+
+""" 
+#####    Hexadecimal
+# Using the function hex() you can convert numbers into a hexadecimal format:
+print(hex(12))
+print(hex(200))
+
+#####    Binary
+# Using the function hex() you can convert numbers into a hexadecimal format:
+print(bin(128))
+print(bin(127))
+
+#####    Exponentials
+# The function pow() takes two arguments, equivalent to x^y. 
+print(pow(2,4))
+print(pow(4,4))
+
+# With three arguments it is equivalent to (x^y)%z, 
+print(pow(2,4,3))
+
+#####    Absolute Value
+# The function abs() returns the absolute value of a number. 
+# The argument may be an integer or a floating point number. If the argument is a complex number, its magnitude is returned.
+print(abs(-3.15))
+print(abs(-3))
+
+#####    Round
+# The function round() will round a number to a given precision in decimal digits (default 0 digits). It does not convert integers to floats.
+print(round(3,2))
+print(round(1395,-3))
+print(round(395,-2))
+print(round(3.14233321,2))
+
+# Python has a built-in math library that is also useful to play around with in case you are ever in need of some mathematical operations.
+#  Explore the documentation here!
+#### https://docs.python.org/3/library/math.html
+"""
+
+# # # # # # # # # #  part 103 ( Advanced Strings) # # # # # # # # #
+
+
+# # # # # # # # # #  part 104 ( Advanced Sets) # # # # # # # # #
+# # # # # # # # # #  part 105 ( Advanced Dictionaries) # # # # # # # # #
+# # # # # # # # # #  part 106 ( Advanced Lists) # # # # # # # # #
+# # # # # # # # # #  part 107 ( Advanced Python Objects Assessment Test) # # # # # # # # #
+# # # # # # # # # #  part 108 ( Advanced Python Objects Test - Solutions) # # # # # # # # #
