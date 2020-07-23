0e65cc1dfc094c65545d65e3233788f378486285
Mouhammad <mouhammad.kharboutli@gmail.com>
2020-07-23 14:55:18 +0200

    added part 48: 10 examples 10 different propblems and their solutions

--- PythonCode/Section 6 Methods and Functions - update.py
+++ PythonCode/Section 6 Methods and Functions - update.py
@@ -199,11 +199,178 @@ print('Thanks for playing with us')
 
 
 
+# # # # # # # # # #  part 48 (EXAMPLES) # # # # # # # # #
 
+### propblem 1 
+"""
+ def my_func():
+    print('Hello world')
+"""
+### propblem 2
 
+"""
+def my_func(fname,lastname):
+    print('Hello {1} {0}'.format(fname,lastname))
 
+my_func('Ahmad','Adi')
+"""
 
+### propblem 3 
+"""
+def my_func(arg1):
+    if arg1 == True:
+        print('Hello')
+    else:
+        print('Good by')
 
+my_func(True)
+"""
+### propblem 4 
 
+"""
+def my_func(x,y,z):
+    if z == True:
+        return x
+    else:
+        return y
+
+print(my_func(1,1000,False))
+"""
+### propblem 5 
+
+"""
+def my_func(a,b):
+    return int(a)+int(b)
+
+print(my_func(1,2))
+print(my_func('1','2'))
+"""
+
+### propblem 6 
+
+"""
+def is_even(num):
+    return num % 2 == 0
+print(is_even(25))
+print(is_even(26))
+"""
+
+### propblem 7 
+""" 
+def is_greater(num1,num2):
+    if num1 > num2:
+        return True
+    else:
+        return False
+
+print(is_greater(3,5))
+"""
+
+#------------------------------------------------------------
+""" 
+# ARGS
+#ARGS is returned as TUPLE
+def sum_numbers(*args):
+    return sum(args)
+
+print(sum_numbers(1,2,3,4,5,6,7))
+
+def show_vars(*args):
+    for item in args:
+        print(item)
+
+show_vars('a',2,3,4,'5','s')
+
+def return_args(*args):
+    return args
+
+print(return_args(1,2,3,4,5,6,6))
+
+### KWARGS
+def choose_fruit(**kwargs):
+    print(kwargs)
+    if 'fruit' in kwargs:
+        print('My favorite fruit is {}'.format(kwargs['fruit']))
+    else:
+        print('I do not know')
+
+choose_fruit(fruit='apple',grocer='lettucd',drink='juice')
+
+### ARGS & KWARGS
+
+def my_func(*args,**kwargs):
+    print(args)
+    print(kwargs)
+
+    if 'fruit' and 'juice' in kwargs:
+        print(f"I would like to have {args[0]} of my favorite {kwargs['fruit']}")
+        print(f"May I have a {kwargs['juice']} juice please !")
+
+        print("I would like to have {} of my favorite {}".format(args[0],kwargs['fruit']))
+
+my_func(22,13,23,10,25,grocery='lettuce',fruit='Pineapple',juice='Orange',film='Heros')   
+"""
+#------------------------------------------------------------
+### propblem 8 
+
+"""
+def my_func(*args):
+    return sum(args)
+
+print(my_func(9,2,5,2,3,5,3,2))
+"""
+
+### propblem 9 
+""" 
+def my_func(*args):
+    result = []
+    for item in args:
+        if item % 2 == 0:
+            result.append(item)
+    return result
+
+print(my_func(1,23,4,5,2,23,5,52,3,23))
+"""
+### propblem 10 
+""" 
+#first way
+def skyLine(x):
+    result = []
+    for i in range(len(x)):
+        if i % 2 == 0:
+            result.append(x[i].lower())
+        else:
+            result.append(x[i].upper())
+    return ''.join(result)
+
+print(skyLine('abdcsdsdksjdkh'))
+
+#Second wat
+def lower_upper(text):
+    elements_list = []
+    result = []
+    for i in text:
+        elements_list.append(i)
+    for index,item in enumerate(elements_list):
+        if index % 2 == 0:
+            result.append(item.upper())
+        else:
+            result.append(item.lower())
+    return ''.join(result)
+
+print(lower_upper('abdcsdsdksjdkh'))
+
+#third way
+def low_up(text):
+    result = ''
+    for x in range(len(text)):
+        if x % 2 == 0:
+            result = result+text[x].upper()
+        else:
+            result = result+text[x].lower()
+    return result 
+
+print(low_up('HelloSOSO'))
+"""
 
 
