aee1a9ad34bdd1170bc5b63e8bb5768a4312d22e
Mouhammad <mouhammad.kharboutli@gmail.com>
2020-07-24 16:47:59 +0200

    Finalzing Section 6 update

--- PythonCode/Section 6 Methods and Functions - update.py
+++ PythonCode/Section 6 Methods and Functions - update.py
@@ -373,4 +373,152 @@ def low_up(text):
 print(low_up('HelloSOSO'))
 """
 
+# https://gist.github.com/Moooodykh/e6c8e562e2a9200bae5901d22aef67ef
+# # # # # # # # # #  part 57-58 (Methods and Functions Homework Overview and Solutions) # # # # # # # # #
+
+#----------------------------------
+
+""" 
+def vol(radius):
+    import math
+    return radius**3 * (4/3) * math.pi
+
+print(vol(2))
+"""
+
+#----------------------------------
+""" 
+def ran_check(num,low,high):
+    if high > num > low:
+        return f'{num} is in the range between {low} and {high}'
+    else:
+        return f'{num} is NOT in the range between {low} and {high}'
+print(ran_check(5,2,7))
+
+def ran_check(num,low,high):
+    return high > num > low 
+print(ran_check(5,2,7))
+
+#----------teacher solution--------
+def ran_bool(num,low,high):
+    return num in range(low,high+1)
+"""
+ #----------------------------------
+# Write a Python function that accepts a string and calculates the number of upper case letters and lower case letters.
+# Sample String : 'Hello Mr. Rogers, how are you this fine Tuesday?'
+# Expected Output : 
+# No. of Upper case characters : 4
+# No. of Lower case Characters : 33
+""" 
+def up_low(text):
+    lower_counter = 0
+    upper_counter = 0
+    for i in range(len(text)):
+        if text[i].isupper():
+            upper_counter +=1
+        elif text[i].islower():
+            lower_counter+=1
+    print('No. of Upper case characters :{}'.format(upper_counter))
+    print('No. of lower case characters :{}'.format(lower_counter))
+
+up_low('Hello Mr. Rogers, how are you this fine Tuesday?')
+ """
+#----------------------------------
+# Write a Python function that takes a list and returns a new list with 
+# unique elements of the first list.
+
+# Sample List : [1,1,1,1,2,2,3,3,3,3,4,5]
+# Unique List : [1, 2, 3, 4, 5]
+
+"""
+def unique_list(lst):
+    return list(set(lst))
+
+print(unique_list([1,1,1,1,2,2,3,3,3,3,4,5]))
+"""
+#----------------------------------
+# Write a Python function to multiply all the numbers in a list.
+
+# Sample List : [1, 2, 3, -4]
+# Expected Output : -24
+
+"""
+def multiply(numbers):  
+    result = 1
+    for num in numbers:
+        result *= num
+    return result
+
+print(multiply([1, 2, 3, -4]))
+"""
+#----------------------------------
+# Write a Python function that checks whether a word or phrase is palindrome or not.
+
+# Note: A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g., madam,kayak,racecar, or a phrase "nurses run". Hint: You may want to check out the .replace() method in a string to help out with dealing with spaces. Also google search how to reverse a string in Python, there are some clever ways to do it with slicing notation.
+
+"""
+def palindrome(word):
+    reserved_item = ''
+    x = reversed(word)
+    for letter in x:
+        reserved_item += letter
+    return reserved_item == word
+    
+def palin(word):
+    return word == word[::-1]
+print(palindrome('madam'))
+print(palin('madam'))
+"""
+
+#----------------------------------
+# Write a Python function to check whether a string is pangram or not. 
+# (Assume the string passed in does not have any punctuation)
+
+# Note : Pangrams are words or sentences containing every letter of the alphabet at least once.
+# For example : "The quick brown fox jumps over the lazy dog"
+
+""" 
+def ispangram(str1):
+    import string
+    alphabet = string.ascii_lowercase
+    return len(set(str1.replace(' ','').lower())) == len(alphabet)
+     # str1.replace : This replaces all spaces ' ' with no space ''. (Fixes issues with strings that have spaces)
+print(ispangram('The quick brown fox jumps over the lazy dog'))
+
+
+def ispang(str1):
+    import string 
+    alphabet = string.ascii_lowercase
+    tmp_list= []
+
+    for item in str1.replace(' ','').lower():
+        if item not in tmp_list and item in alphabet:
+            tmp_list.append(item)
+    return len(tmp_list) == len(alphabet)
+    
+print(ispang('The quick brown fox jumps over the lazy dog'))
+
+#---------------TEACHER SOLUTION-------------------
+import string
+
+def ispangram(str1, alphabet=string.ascii_lowercase): 
+    # Create a set of the alphabet
+    alphaset = set(alphabet)  
+    
+    # Remove spaces from str1
+    str1 = str1.replace(" ",'')
+    
+    # Lowercase all strings in the passed in string
+    # Recall we assume no punctuation 
+    str1 = str1.lower()
+    
+    # Grab all unique letters in the string as a set
+    str1 = set(str1)
+    
+    # Now check that the alpahbet set is same as string set
+    return str1 == alphaset
+"""
+#----------------------------------
+
+
 
