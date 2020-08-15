c4ccca4451da05ecaf10d3a876ed0da248bad9ab
== <==>
2020-08-15 09:54:41 +0200

    added Python Collections Module part 103 (Methods and the Python Documentation) part 104 (Python Collections Module):- COUNTER CLASS

--- PythonCode/Section 14 Advanced Python Modules - Update.py
+++ PythonCode/Section 14 Advanced Python Modules - Update.py
@@ -1,4 +1,81 @@
 # ------------------------ Section 14 "UPDATE" is from part 103-115   -----------------------------
 
 # # # # # # # # # #  part 103 (Methods and the Python Documentation) # # # # # # # # #
+# MODULES
 
+# # # # # # # # # #  part 104 (Python Collections Module) # # # # # # # # #
+
+
+################### Counter class ########################
+# we use it to count how many times a list member shows up
+# Counter is dictionary sub class , which means it is a dictionary at the end
+from collections import Counter
+
+mylist = [1,1,1,2,3,4,3,3,3,4,2,2,1,4,5,1]
+mylist2 = ['a','a',5,2,2,1,1,5,1]
+x = Counter(mylist2)
+print(Counter(mylist))
+print(Counter(mylist2))
+print(x['a'])
+
+# we can count also strings
+word ='aaasvvvssddsssdsddsdsb'
+print(Counter(word))
+
+# we can count also words in a sentence but do not forget to call split function
+sentence = 'How many words has been shown with word word'
+print(Counter(sentence.split()))
+
+####Special methods with Counter
+letters = 'aaaaaaabbbbbsssssssdddddddeeeeeeeeeeee'
+c = Counter(letters)
+#Most common: will return the result as TUPLE sorted by the most item repeated and descently
+print(c.most_common())
+print(c.most_common(3))# 3 means give the most 3 common items.
+
+
+####this is a list about what we can do
+# sum(c.values())                 # total of all counts
+# c.clear()                       # reset all counts
+# list(c)                         # list unique elements
+# set(c)                          # convert to a set
+# dict(c)                         # convert to a regular dictionary
+# c.items()                       # convert to a list of (elem, cnt) pairs
+# Counter(dict(list_of_pairs))    # convert from a list of (elem, cnt) pairs
+# c.most_common()[:-n-1:-1]       # n least common elements
+# c += Counter()                  # remove zero and negative counts
+
+print(c.values())# Give you all counts for each element as a LIST  a=7,b=5....etc.
+c.clear() #reset all counts
+print(c.values())# Give you all counts for each element a=7,b=5....etc.
+c = Counter(letters)
+print(list(c))# Give you all uniqe elements as List
+print(set(c))# Give you all uniqe elements as SET
+print(dict(c))# Give you all elements and counts as DICT
+print(c.items())# Give you a list whch contains of PAIRS (element,count)
+
+
+list_of_pairs = c.items()
+print(Counter(dict(list_of_pairs))) #convert from a list of (elem, cnt) pairs
+print((dict(list_of_pairs)))
+print(c.most_common()[::-1]) # give us all elements ordered from least common element
+print(c.most_common()[-1::]) #give us least common elements as LIST
+print(c.most_common()[-1]) #give us least common elements as ELEMENT (TUPLE) 
+print('*'*100)
+
+
+################### Counter class ########################
+
+
+
+# # # # # # # # # #  part 105 (Methods and the Python Documentation) # # # # # # # # #
+# # # # # # # # # #  part 106 (Methods and the Python Documentation) # # # # # # # # #
+# # # # # # # # # #  part 107 (Methods and the Python Documentation) # # # # # # # # #
+# # # # # # # # # #  part 108 (Methods and the Python Documentation) # # # # # # # # #
+# # # # # # # # # #  part 109 (Methods and the Python Documentation) # # # # # # # # #
+# # # # # # # # # #  part 110 (Methods and the Python Documentation) # # # # # # # # #
+# # # # # # # # # #  part 111 (Methods and the Python Documentation) # # # # # # # # #
+# # # # # # # # # #  part 112 (Methods and the Python Documentation) # # # # # # # # #
+# # # # # # # # # #  part 113 (Methods and the Python Documentation) # # # # # # # # #
+# # # # # # # # # #  part 114 (Methods and the Python Documentation) # # # # # # # # #
+# # # # # # # # # #  part 115 (Methods and the Python Documentation) # # # # # # # # #
