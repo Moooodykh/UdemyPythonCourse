0f3865cca135611a10f85a0b627babd7901ad484
== <==>
2020-09-13 22:10:41 +0200

    added part 136 (Receiving Emails with Python)

--- PythonCode/Section 18 Emails with Python.py
+++ PythonCode/Section 18 Emails with Python.py
@@ -1,10 +1,11 @@
 # ------------------------ Section 18: Working with Emails is from part 134 to 136   -----------------------------
 
-# # # # # # # # # #  part 134 ( Advanced Dictionaries) # # # # # # # # #
+# # # # # # # # # #  part 134 ( Introduction to Emails with Python) # # # # # # # # #
 # will be using SMTP library
 #will try to work with GMAIL mail service
 
-# # # # # # # # # #  part 135 ( Advanced Lists) # # # # # # # # #
+# # # # # # # # # #  part 135 ( Sending Emails with Python) # # # # # # # # #
+""" 
 import smtplib
 
 smtp_object = smtplib.SMTP('smtp.gmail.com',587) #this is the protcols that big mail comapnies are using
@@ -48,13 +49,55 @@ smtp_object.sendmail(from_adress,to_adress,msg)
 # now you need to close the connection
 smtp_object.quit()
 #(221, b'2.0.0 closing connection j1sm22376227pgq.33 - gsmtp') should be the response
+"""
 
 
+# # # # # # # # # #  part 136 (Receiving Emails with Python) # # # # # # # # #
+# we will deal with Imaplibrary and email library
+import imaplib
+import  email
+import  getpass
 
+M = imaplib.IMAP4_SSL('imap.gmail.com')
 
+email = getpass.getpass('Email please: ')
+password = getpass.getpass('Password please: ')
+print(M.login(email,password))
 
+print(M.list())
+M.select("Sent")
+# M.select("inbox")
 
+# Searching Mail
+# Now that we have connected to our mail, we should be able to search for it using the specialized syntax of IMAP. 
+# Here are the different search keys you can use:
+#-------------------------------------------------------------------------------------------------------------------------------
+# Keyword	                                                    Definition
+# 'ALL'	                                                        Returns all messages in your email folder. Often there are size limits from imaplib. To change these use imaplib._MAXLINE = 100 , where 100 is whatever you want the limit to be.
+# 'BEFORE date'	                                                Returns all messages before the date. Date must be formatted as 01-Nov-2000.
+# 'ON date'         	                                        Returns all messages on the date. Date must be formatted as 01-Nov-2000.
+# 'SINCE date'      	                                        Returns all messages after the date. Date must be formatted as 01-Nov-2000.
+# 'FROM some_string '	                                        Returns all from the sender in the string. String can be an email, for example 'FROM user@example.com' or just a string that may appear in the email, "FROM example"
+# 'TO some_string'  	                                        Returns all outgoing email to the email in the string. String can be an email, for example 'FROM user@example.com' or just a string that may appear in the email, "FROM example"
+# 'CC some_string' and/or 'BCC some_string'                     Returns all messages in your email folder. Often there are size limits from imaplib. To change these use imaplib._MAXLINE = 100 , where 100 is whatever you want the limit to be.
+# 'SUBJECT string','BODY string','TEXT "string with spaces"'	Returns all messages with the subject string or the string in the body of the email. If the string you are searching for has spaces in it, wrap it in double quotes.
+# 'SEEN', 'UNSEEN'	                                            Returns all messages that have been seen or unseen. (Also known as read or unread)
+# 'ANSWERED', 'UNANSWERED'	                                    Returns all messages that have been replied to or unreplied to.
+# 'DELETED', 'UNDELETED'	                                    Returns all messages that have been deleted or that have not been deleted.
 
+# Use if you get an error saying limit was reached
+imaplib._MAXLINE = 10000000
+typ ,data = M.search(None,'SUBJECT "Python test"')
 
+print(typ)
+print(data)
+result, email_data = M.fetch(data[0],"(RFC822)")
+raw_email = email_data[0][1]
+raw_email_string = raw_email.decode('utf-8')
 
-# # # # # # # # # #  part 136 ( Advanced Lists) # # # # # # # # #
+email_message = email.message_from_string(raw_email_string)
+
+for part in email_message.walk():
+    if part.get_content_type() == "text/plain":
+        body = part.get_payload(decode=True)
+        print(body)
