f5fb52a98cd16920bb7322356d9d0ca6306b09c1
== <==>
2020-09-26 16:13:08 +0200

    added : - Teacher solution for part 123-124 - Course slides file

--- PythonCode/Section 15 Web Scraping with Python.py
+++ PythonCode/Section 15 Web Scraping with Python.py
@@ -262,4 +262,32 @@ while run:
 print(unique_authors)
 print(len(unique_authors))
 
+
+#### TEACHER SOLUTION
+page_still_valid = True
+authors = set()
+page = 1
+
+while page_still_valid:
+
+    # Concatenate to get new page URL
+    page_url = url+str(page)
+    
+    # Obtain Request
+    res = requests.get(page_url)
+    
+    # Check to see if we're on the last page
+    if "No quotes found!" in res.text:
+        break
+    
+    # Turn into Soup
+    soup = bs4.BeautifulSoup(res.text,'lxml')
+    
+    # Add Authors to our set
+    for name in soup.select(".author"):
+        authors.add(name.text)
+        
+    # Go to Next Page
+    page += 1
+    
 """
